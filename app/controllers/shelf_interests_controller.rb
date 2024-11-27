404800be17817d6abcbf009f396ac24a370cf46c
mason kamolpechara <mason.kamolpechara@gmail.com>
2024-11-27 11:31:44 +0900

    search bar interests index create shelfinterest done

--- app/controllers/shelf_interests_controller.rb
+++ app/controllers/shelf_interests_controller.rb
@@ -11,21 +11,22 @@ class ShelfInterestsController < ApplicationController
 
   def create
     #I seem to have shelf_interest passed in as a parameter from the form, but dont know how to read it
-
-    @shelf_interest = ShelfInterest.new(shelf_interest_params)
-    # @shelf_interest.interest = interest
-    # @shelf_interest.user = current_user
-
+    @shelf_interest = ShelfInterest.new
+    # @shelf_interest = ShelfInterest.new(shelf_interest_params)
+    @interest = Interest.find(params[:shelf_interest][:interest_id])
+    
+    @shelf_interest.interest = @interest
+    @shelf_interest.user = current_user
 
     if @shelf_interest.save
-      redirect_to user_self_interest_path(current_user)
+      redirect_to user_self_interests_path(current_user, :media_type => @interest.media_type)
     else
       raise # shouldnt ever get here
     end
   end
 
-  private 
-  def shelf_interest_params
-    params.require(:shelf_interest).permit(:user_id, :interest_id)
-  end
+  # private 
+  # def shelf_interest_params
+  #   params.require(:shelf_interest).permit(:interest_id)
+  # end
 end
