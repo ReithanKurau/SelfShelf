0e9e4ff77d9502bcd6447306278ee384b92032f7
Your Name <reithankurau@gmail.com>
2024-11-29 15:47:35 +0900

    Problems solved

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,10 +1,7 @@
-// app/assets/config/manifest.js
-// ...
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
+//= link popper.js
+//= link bootstrap.min.js
 //= link manifest.json
-//= link application.css
-//= link application.js
-//= link controllers/application.js
-//= link controllers/hello_controller.js
-//= link controllers/index.js
-//= link controllers/movies_controller.js
-//= link no_cover.gif
