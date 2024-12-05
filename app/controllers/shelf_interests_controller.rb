8ab20b1672c3fb863204f50550477195906fddec
Phyu Phyu Hlaing <phyuphyu.h27@gmail.com>
2024-12-05 15:01:05 +0900

    add only favs to the shelf

--- app/controllers/shelf_interests_controller.rb
+++ app/controllers/shelf_interests_controller.rb
@@ -14,10 +14,12 @@ class ShelfInterestsController < ApplicationController
       @movies = @user.interests.where(media_type: 'movie')
       @albums = @user.interests.where(media_type: 'album')
 
+      @fav_books = @user.favorited_by_type('ShelfInterest').map { |si| si.interest if si.interest.media_type == 'book' }
+
       render "index"
     end
-    
-    @favorite_books = @user.all_favorites.where(media_type: 'book')
+
+
     @favorite_movies = @user.all_favorites.where(media_type: 'movie')
     @favorite_albums = @user.all_favorites.where(media_type: 'album')
 
